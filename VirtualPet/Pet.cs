a69a8861b871b49f64577c5d2d98eb118289e561
Chris Lawver <chris_lawver@yahoo.com>
2021-02-01 15:16:14 -0500

    Completed first few tests

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -6,5 +6,32 @@ namespace VirtualPet
 {
     public class Pet
     {
+        public string Name { get; set; }
+        public string Species { get; set; }
+
+        public void SetName(string name)
+        {
+            Name = name;
+            
+        }
+
+        public string GetName()
+        {
+            
+            return Name;
+        }
+
+        public void SetSpecies(string species)
+        {
+            Species = species;
+
+        }
+
+        public string GetSpecies()
+        {
+
+            return Species;
+        }
+
     }
 }
