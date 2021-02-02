a405984601606c33273c0bd3068d1e5cc59f8fd7
Joseph Erxleben <jerxleben.13@gmail.com>
2021-02-02 10:52:50 -0500

    Finished adding instance variables and methods for tests, all tests pass.

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -8,6 +8,9 @@ namespace VirtualPet
     {
         public string Name { get; set; }
         public string Species { get; set; }
+        public int Hunger { get; set; } = 50;
+        public int Boredom { get; set; } = 60;
+        public int Health { get; set; } = 30;
 
         public void SetName(string name)
         {
@@ -33,5 +36,44 @@ namespace VirtualPet
             return Species;
         }
 
+        public int GetHunger()
+        {
+
+            return Hunger;
+        }
+        public int GetBoredom()
+        {
+
+            return Boredom;
+        }
+        public int GetHealth()
+        {
+
+            return Health;
+        }
+
+        public void Feed()
+        {
+            Hunger -= 40;
+        }
+
+        public void SeeDoctor()
+        {
+            Health += 30;
+        }
+
+        public void Play()
+        {
+            Hunger += 10;
+            Boredom -= 20;
+            Health += 10;
+        }
+
+        public void Tick()
+        {
+            Hunger += 5;
+            Boredom += 5;
+            Health -= 5;
+        }
     }
 }
